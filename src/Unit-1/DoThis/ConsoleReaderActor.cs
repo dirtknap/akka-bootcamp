c16692bdd38d55f4f7e192dedc685fdc25f4895c
Jeff Rector <jeff.rector@incontact.com>
2016-04-05 16:51:10 -0400

    Lesson 2 complete

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,8 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
+
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,21 +21,60 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
             }
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            GetAndValidateInput();
         }
+
+        #region Private Methods
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write data to console\n");
+            Console.WriteLine("Type exit to quit\n");
+        }
+
+
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                Context.System.Terminate();
+            }
+            else
+            {
+                if (IsValidMessage(message))
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Message is valid"));
+
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: Input had odd number of characters"));
+                }
+            }
+        }
+
+        private bool IsValidMessage(string message)
+        {
+            return message.Length%2 == 0;
+        }
+
+        #endregion
+
 
     }
 }
---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,62 @@
+﻿namespace WinTail
+{
+    public class Messages
+    {
+
+        #region Neutral/System Messages
+ 
+        public class ContinueProcessing
+        {
+             
+        }
+
+        #endregion
+
+
+        #region Success Messages
+
+        public class InputSuccess 
+        {
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+
+        #endregion
+
+        #region Error Messages
+
+        public class InputError
+        {
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+
+            public string Reason { get; private set; }
+        }
+
+
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason)
+            {
+            }
+        }
+
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason)
+            {
+            }
+        }
+
+        #endregion
+
+
+    }
+}
